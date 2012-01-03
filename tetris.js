bdfb481ceebd6836dbc86ad92f93074e9f689aea
Sam <sam@ubuntu.ubuntu-domain>
2012-01-02 23:26:46 -0500

    collisions

--- tetris.js
+++ tetris.js
@@ -1,11 +1,45 @@
 var ctx;
 var tick;
 var blockSpeed = 200/5000;
+var blockWidth = 20;
 var entities = [];
+var z = [];
+
+var grid = {
+  g: _.range(30).map(function(i) {return _.range(11).map(function(j) { return i < 15 ? 1 : 0 })}),
+  tiles: [[]],
+  set: function(row,col,color) {this.g[row][col] = color},
+  update: function(delta) {
+    this.render();
+  },
+  render: function() {
+    for(var i=0; i<30; i++) {
+      for(var j=0; j<30; j++) {
+        this.tiles[i][j].color = this.g[i][j] == 0 ? "black" : "white";
+      }
+    }
+  }
+}
+for(var i=0; i<30; i++) {
+  grid.tiles[i] = [];
+  for(var j=0; j<30; j++) {
+    var t = tile(j * blockWidth, i * blockWidth, blockWidth, blockWidth, grid.g[i][j]);
+    z.push(t);
+    grid.tiles[i][j] = t;
+  }
+}
+entities.push(grid);
+
+function collide(t1, t2) {
+  return(
+    ((t1.x > t2.x + t2.width)  || (t1.x + t1.width  < t2.x) ||
+    (t1.y > t2.y + t2.height) || (t1.y + t1.height < t2.y)) ?
+    false : true);
+}
 
 function draw() {
   ctx.clearRect(0,0,220,600);
-  _.invoke(z, 'draw', ctx);
+  _(z).invoke('draw', ctx);
 }  
 
 function update(t) {
@@ -13,7 +47,7 @@ function update(t) {
   if(!tick) { tick = t; }
   var delta = t - tick;
   tick = t;
-  _.invoke(entities, 'update', delta);
+  _(entities).invoke('update', delta);
   draw();
 }
 
@@ -29,13 +63,15 @@ return  window.requestAnimationFrame       ||
       };
 })()
 
-function tile(x, y, width, height) {
+function tile(x, y, width, height, color) {
  return({
    x: x,
    y: y,
    width: width,
    height: height,
+   color: color || 'black',
    draw: function(ctx) {
+     ctx.fillStyle = this.color;
      ctx.fillRect(this.x, this.y, this.width, this.height);
    }
  });
@@ -44,8 +80,8 @@ function tile(x, y, width, height) {
 function block() {
   var tiles = [];
   for(var i=0; i<4; i++) {
-    var t = tile(0,0,10,10);
-    tiles.push({tile: t, offset: {x: 10 * i, y: 0}});
+    var t = tile(0,0,blockWidth,blockWidth);
+    tiles.push({tile: t, offset: {x: blockWidth * i, y: 0}});
     z.push(t);
   }
   return({
@@ -55,17 +91,18 @@ function block() {
     update: function(delta) {
       this.y = this.y + blockSpeed * delta;
       this.render();
+      if(_(tiles).chain().map(function(t){return t.tile;}).any(function(t1) {return _(grid.tiles).chain().flatten().any(function(t2) { return collide(t1, t2) && t1.color == t2.color }).value();}).value()) {
+        console.log("collision");
+      }
     },
     render: function() {
-      _.each(this.tiles, function(t) {
+      _(this.tiles).each(function(t) {
         t.tile.x = this.x + t.offset.x;
         t.tile.y = this.y + t.offset.y;
       }, this);
     }
   }); 
 }
-
-var z = [];
 
 window.addEventListener("DOMContentLoaded", function(){
   ctx = document.querySelector("canvas").getContext("2d");
